6bc12c8868ec5a38c9612fa2623835ad2f3a89f6
Saurabh Pathak <saurabh.pathak@bdo.ext.global>
2026-02-04 12:33:17 +0530

    remove result page

--- src/app/result/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const ResultClient = dynamic(() => import("@/app/result/ResultClient"));
-
-export default function ResultPage() {
-    return <ResultClient />;
-}
